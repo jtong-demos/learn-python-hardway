d3de8fd6afff7a76d207248705dafb79e79ab642
Jian Tong <tj19832@gmail.com>
2018-08-09 00:21:25 +0800

    ex 3,4,5

---
+++ python-3/ex5.py
@@ -0,0 +1,9 @@
+my_name = 'Zed A. Shaw'
+print("Let's talk about %s." % my_name)
+
+my_age = 35 # not a lie
+my_height = 74 # inches
+my_weight = 180 # lbs
+
+print("If I add %d, %d, and %d I get %d." % (
+    my_age, my_height, my_weight, my_age + my_height + my_weight))
