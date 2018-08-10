2d3bf84b67aef5469580281b5b59da4755565d29
Jian Tong <tj19832@gmail.com>
2018-08-11 00:26:39 +0800

    ex8,ex9

---
+++ python-3/ex8.py
@@ -0,0 +1,13 @@
+formatter = "%r %r %r %r"
+
+print(formatter % (1,2,3,4))
+print(formatter % ("one", "two", "three","four"))
+print(formatter % (formatter, formatter, formatter, formatter))
+print(formatter % (
+    "sentence 1",
+    "sentence 2",
+    "sentence 3",
+    "sentence 4"
+))
+
+print("%s" %("one"))
